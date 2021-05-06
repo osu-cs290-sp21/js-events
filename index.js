19c64e51c1175b6a833a1fd499efc2b9000db5fc
Rob Hess <hessro@oregonstate.edu>
2021-05-06 15:50:14 -0700

    Add code from 5/6 lecture.

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-window.addEventListener('click', function () {
+window.addEventListener('click', function (event) {
   console.log("== The window was clicked")
+  console.log("  -- event.target:", event.target)
+})
+
+var firstBox = document.querySelector('.box');
+firstBox.addEventListener('click', function (event) {
+  console.log("== The first box was clicked")
+  console.log("  -- event.target:", event.target)
+  console.log("  -- event.currentTarget:", event.currentTarget)
+  // event.stopPropagation()
+})
+
+function buttonClickListener(event) {
+  console.log("== A button was clicked, event:", event)
+  console.log("  -- event.currentTarget:", event.currentTarget)
+  var box = event.currentTarget.parentNode
+  box.classList.toggle('highlighted')
+  // return function () {
+  //   console.log("== The function returned by buttonClickListener()!!")
+  // }
+}
+
+var buttons = document.getElementsByClassName('in-box-button')
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', buttonClickListener)
+}
+
+function foo() {
+  console.log("== inside foo")
+  return "foo's return value"
+}
+
+console.log("== foo():", foo())
+console.log("== foo:", foo)
+
+document.getElementById('website-link').addEventListener('click', function (event) {
+  console.log("== The link was clicked")
+  event.preventDefault()
 })
--- wordBoxes.js
+++ wordBoxes.js
@@ -1,12 +1,21 @@
 var allWords = [];
 var currentWord = 0;
 
+var wordsInput = document.getElementById('words-input')
+
 function handleNewWordsEntered(event) {
-  var text = '';
+  console.log("== handleNewWordsEntered() was called")
+  var text = wordsInput.value;
+  console.log("  -- text:", text)
   allWords = text.replace(/[!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~]/g, '')
     .toLowerCase().split(' ');
   currentWord = 0;
+  console.log("  -- allWords:", allWords)
+  // wordsInput.value = ''
 }
+
+// wordsInput.addEventListener('input', handleNewWordsEntered)
+wordsInput.addEventListener('change', handleNewWordsEntered)
 
 function generateWordElem(word, highlightColor) {
 	var wordElem = document.createElement('span');
@@ -17,4 +26,30 @@ function generateWordElem(word, highlightColor) {
   	wordElem.classList.add(highlightColor);
   }
   return wordElem;
+}
+
+var addWordButtons = document.getElementsByClassName('add-word-button')
+
+function handleAddWordButtonClick(event) {
+  console.log("== handleAddWordButtonClick() was called, event.currentTarget:",
+    event.currentTarget)
+
+  var word = allWords[currentWord]
+  if (word) {
+    var wordElem = generateWordElem(word)
+    console.log("  -- wordElem:", wordElem)
+
+    var container = event.currentTarget.parentNode.parentNode
+    var wordsContainer = container.querySelector('.words-container')
+    console.log("  -- container:", container)
+    console.log("  -- wordsContainer:", wordsContainer)
+
+    wordsContainer.appendChild(wordElem)
+
+    currentWord = (currentWord + 1) % allWords.length
+  }
+}
+
+for (var i = 0; i < addWordButtons.length; i++) {
+  addWordButtons[i].addEventListener('click', handleAddWordButtonClick)
 }
